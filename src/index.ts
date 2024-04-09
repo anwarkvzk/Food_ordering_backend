59ba5b5e580f48abc111fbcc3178ad4886c28c43
anwarkvzk <anvarvzk521@gmail.com>
2024-04-09 09:56:54 +0530

    User model created

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
-import express,{Request, Response} from "express";
-import cors from "cors"
-import "dotenv/config"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to Database"));
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello!" });
+});
 
-app.get('/test', async (req: Request, res: Response)=>{
-    res.json({message: 'Hello!'})
-})
-
-app.listen(7000, ()=>{
-    console.log("Server Started on localhost : 7000")
-})
+app.listen(7000, () => {
+  console.log("Server Started on localhost : 7000");
+});
